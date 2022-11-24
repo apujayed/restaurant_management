dfe108e023e9085083ea45e7749d29b2c7745c64
Azim Uddin Ahamed <azimuahmed13@gmail.com>
2022-11-25 02:20:22 +0600

    fixed alignment

--- src/components/FilterMenu.js
+++ src/components/FilterMenu.js
@@ -2,8 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import categories from "../data/category";
 import { filterfood } from "../store/productsSlice";
 import { useDispatch, useSelector } from "react-redux";
-import { Box } from '@mui/material';
-import { Button } from '@mui/material';
+import { Box, Button } from '@mui/material';
 const FilterMenu = () => {
   const dispatch = useDispatch();
   const products = useSelector((state) => state.products);
@@ -27,55 +26,51 @@ const FilterMenu = () => {
   }, [scrollX]);
 
   return (
-    <>
-      <div className="row">
-        <div className="col-sm-8">
-          <div class="slide-sample">
-            <div id="slideRight" onClick={() => handleScroll({ side: "left" })} className="preSlide">
-              <i className="fas fa-caret-square-left" />
-            </div>
-            <div ref={scrollLeftRef} class="slideouter">
-              <div class="slideinner srcl">
-                <ul>
-                  {
-                    categories.map((category, i) => {
-                      return (
-
-                        <Button
-                          key={category.id}
-                          style={{ marginRight: "8px" }}
-                          onClick={() => { dispatch(filterfood(category.name)); handleItemClick(i) }}
-                          variant={activeclass === i ? "contained" : "outlined"}
-                          size="medium"
-                        >
-                          {category.name}
-                        </Button>
-                      )
-                    })}
-                </ul>
-              </div>
-            </div>
-            <div id="slideRight" onClick={() => handleScroll({ side: "right" })} className="nextSlide">
-
-              <i className="fas fa-caret-square-right" />
-            </div>
-
+    <Box className="row">
+      <div className="col-sm-8">
+        <div class="slide-sample">
+          <div onClick={() => handleScroll({ side: "left" })} className="preSlide">
+            <i className="fas fa-caret-square-left" />
           </div>
+          <div ref={scrollLeftRef} class="slideouter">
+            <div class="slideinner srcl">
+              <ul>
+                {
+                  categories.map((category, i) => {
+                    return (
+                      <Button
+                        key={category.id}
+                        style={{ marginRight: "8px" }}
+                        onClick={() => { dispatch(filterfood(category.name)); handleItemClick(i) }}
+                        variant={activeclass === i ? "contained" : "outlined"}
+                        size="medium"
+                      >
+                        {category.name}
+                      </Button>
+                    )
+                  })}
+              </ul>
+            </div>
+          </div>
+          <div onClick={() => handleScroll({ side: "right" })} className="nextSlide">
+            <i className="fas fa-caret-square-right" />
+          </div>
+
         </div>
-        <div id="search" className="py-3 col-sm-4 col-12 d-flex justify-content-center align-items-center">
-          <div className="row d-flex">
-            <div className="px-0 col-sm-8 col-8">
-              <input type="text" className="form-control" value="" />
-            </div>
-            <div className="px-0 col-sm-4 col-4">
-              <button type="button" class="btn btn-primary">
-                Search
-              </button>
-            </div>
+      </div>
+      <div id="search" className="py-3 col-sm-4 col-12 d-flex justify-content-center align-items-center">
+        <div className="row d-flex">
+          <div className="px-0 col-sm-8 col-8">
+            <input type="text" className="form-control" value="" />
+          </div>
+          <div className="px-0 col-sm-4 col-4">
+            <button type="button" class="btn btn-primary">
+              Search
+            </button>
           </div>
         </div>
       </div>
-    </>
+    </Box>
   );
 };
 
