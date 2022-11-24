032eb9162b6c11d56521cc6356437675279adee6
Jayed Apu <95185551+apujayed@users.noreply.github.com>
2022-11-24 16:50:03 +0600

    pusher added

--- src/containers/FoodMenu.js
+++ src/containers/FoodMenu.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect ,useState} from "react";
 import "../food.css";
 import banner from "../images/banner.png";
 import FilterMenu from "../components/FilterMenu";
@@ -7,10 +7,30 @@ import CartFood from "./CartFood";
 import Footer from "../components/Footer";
 import AliceCarousel from 'react-alice-carousel';
 import "react-alice-carousel/lib/alice-carousel.css";
+import Pusher from 'pusher-js';
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts } from "../store/productsSlice";
 const FoodMenu = () => {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.products);
 
+  // useEffect(() => {
+  //   dispatch(fetchProducts());
+  // }, [dispatch]);
 
+ const [msg, setMsg] = useState([]);
+ useEffect(()=>{
+  const pusher = new Pusher('4878a7a1d217f890b537', {
+    cluster: 'mt1',
+    encrypted: true
+  });
+  const channel = pusher.subscribe('chat');
+  channel.bind('message', data => {
+    alert('You have a new order')
+    dispatch(fetchProducts());
  
+  });
+ },[])
 
   useEffect(() => {
     const buttonRight = document.getElementById("slideRight");
@@ -33,7 +53,6 @@ const FoodMenu = () => {
 </AliceCarousel>
       {/* <img src={banner} class="img-fluid banner" alt="Responsive image" /> */}
         <div className="food-body">
-        
           <FilterMenu />
           <div className="row">
             <FoodItemContainer />
